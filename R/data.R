0536d82d443e693e26ac97db8604b86ac25d6fa0
Frank Höger <frank.hoeger@mirsem.org>
2020-01-13 21:32:45 +0100

    add PAC; refactor code

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' DAC  dictionary
+#' DAC dictionary
 #'
 #' The default internationalisation dictionary used by the DAC
 #' @name DAC_dict
@@ -9,6 +9,21 @@ NULL
 #'
 #' The DAC's item bank
 #' @name DAC_item_bank
+#' @docType data
+NULL
+
+
+#' PAC dictionary
+#'
+#' The default internationalisation dictionary used by the PAC
+#' @name PAC_dict
+#' @docType data
+NULL
+
+#' PAC item bank
+#'
+#' The PAC's item bank
+#' @name PAC_item_bank
 #' @docType data
 NULL
 
--- R/main_test.R
+++ R/main_test.R
@@ -1,23 +1,32 @@
-
-DAC_main_test <- function(label, num_items, offset = 1) {
+main_test <- function(questionnaire, label, num_items, offset = 1) {
   elts <- c()
   num_options <- 5
-  for(item_id in (offset + 1):(offset + num_items)){
+  for (item_id in (offset + 1):(offset + num_items)) {
     label <- sprintf("q%d", item_id - offset)
     choices <- sprintf("btn%d_text", 1:num_options)
-    choice_ids <- sprintf("TDAC_000%d_CHOICE%d", item_id, 1:num_options)
+    choice_ids <-
+      sprintf("T%s_000%d_CHOICE%d", questionnaire, item_id, 1:num_options)
     item_page <- psychTestR::new_timeline(
-      psychTestR::NAFC_page(label = label,
-                            prompt = get_prompt(item_id - offset, num_items, sprintf("TDAC_000%d_PROMPT", item_id)),
-                            choices = choices,
-                            labels = map(choice_ids, psychTestR::i18n)),
-      dict = psyquest::psyquest_dict)
+      psychTestR::NAFC_page(
+        label = label,
+        prompt = get_prompt(
+          item_id - offset,
+          num_items,
+          sprintf("T%s_000%d_PROMPT", questionnaire,  item_id)
+        ),
+        choices = choices,
+        labels = map(choice_ids, psychTestR::i18n)
+      ),
+      dict = psyquest::psyquest_dict
+    )
     elts <- c(elts, item_page)
   }
   psychTestR::join(elts)
 }
 
-get_prompt <- function(item_number, num_items_in_test, prompt_id) {
+get_prompt <- function(item_number,
+                       num_items_in_test,
+                       prompt_id) {
   shiny::div(
     shiny::h4(
       psychTestR::i18n(
