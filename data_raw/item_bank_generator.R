5244335850f42097d33c1526a340d17f862ce470
Frank Höger <frank.hoeger@mirsem.org>
2020-01-14 22:39:02 +0100

    add item_bank_generator /2

--- data_raw/item_bank_generator.R
+++ data_raw/item_bank_generator.R
@@ -1,6 +1,9 @@
 psyquest_item_bank <-
-  map_dfr(list.files("./data_raw/item_banks/", full.names = TRUE), function(x) readRDS(x))
+  map_dfr(list.files("./data_raw/item_banks/", full.names = TRUE), function(x)
+    readRDS(x))
 
-psyquest_item_bank <- psyquest_item_bank %>% as_tibble() %>% filter(str_detect(language, "en"), str_detect(score_func, "NA", negate = TRUE)) %>% select(main_id, score_func, subscales)
+psyquest_item_bank <-
+  psyquest_item_bank %>% as_tibble() %>% filter(str_detect(language, "en"),
+                                                str_detect(score_func, "NA", negate = TRUE)) %>% select(main_id, score_func, subscales) %>% rename(prompt_id = main_id)
 
 usethis::use_data(psyquest_item_bank, overwrite = TRUE)
