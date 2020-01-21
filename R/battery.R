777e80760df77f79f75e7d908a18342556638cf8
Frank Höger <frank.hoeger@mirsem.org>
2020-01-21 16:50:28 +0100

    add battery function and basic test

---
+++ R/battery.R
@@ -0,0 +1,48 @@
+#' battery
+#'
+#' This function defines a battery, a series of questionnaires.
+#' Use this function if you want to create a battery of tests.
+#' For demoing the battery, consider using \code{\link{battery_demo}()}.
+#' @param label (Character scalar) Label to give the DAC results in the output file.
+#' @param dict The psyquest dictionary used for internationalisation.
+#' @export
+battery <- function(title = "LongGold Test Umbrella Test",
+                    documentation = "LGTUT",
+                    admin_password = "conifer",
+                    researcher_email = "musicsophistication@gmail.com",
+                    languages = c("EN", "DE"),
+                    validate_id = "auto",
+                    dict = psyquest::psyquest_dict,
+                    questionnaires = questionnaires,
+                    ...) {
+  elts <- c(register_participant(validate_id, dict))
+  elts <- append(elts, c(questionnaires))
+  elts <- append(elts, c(psychTestR::elt_save_results_to_disk(complete = TRUE)))
+  elts <- append(elts,
+    c(psychTestR::new_timeline(
+      psychTestR::final_page(shiny::p(
+        psychTestR::i18n("RESULTS_SAVED"),
+        psychTestR::i18n("CLOSE_BROWSER"))
+      ), dict = dict
+    ))
+  )
+
+  psychTestR::make_test(
+    elts,
+    opt = psychTestR::test_options(title = title,
+                                   admin_password = admin_password,
+                                   researcher_email = researcher_email,
+                                   demo = FALSE,
+                                   languages = languages))
+}
+
+register_participant <- function(validate_id, dict) {
+  psychTestR::new_timeline(
+    psychTestR::get_p_id(
+      prompt = psychTestR::i18n("ENTER_ID"),
+      button_text = psychTestR::i18n("CONTINUE"),
+      validate = validate_id
+    ),
+    dict = dict
+  )
+}
--- R/main_test.R
+++ R/main_test.R
@@ -1,11 +1,18 @@
 main_test <- function(questionnaire, label, num_items, offset = 1, arrange_vertically = TRUE) {
   elts <- c()
+  elts <- c(elts, psychTestR::new_timeline(
+      psychTestR::one_button_page(
+        body = psychTestR::i18n(stringr::str_interp("T${questionnaire}_0001_PROMPT")),
+        button_text = psychTestR::i18n("CONTINUE")
+      ),
+      dict = psyquest::psyquest_dict
+    )
+  )
   for (item_id in (offset + 1):(offset + num_items)) {
     label <- sprintf("q%d", item_id - offset)
     item_bank_row  <-
       psyquest::psyquest_item_bank %>%
-      filter(stringr::str_detect(prompt_id,
-                        sprintf("T%s_%04d", questionnaire, item_id)))
+      filter(stringr::str_detect(prompt_id, sprintf("T%s_%04d", questionnaire, item_id)))
     num_of_options <- strsplit(item_bank_row$option_type, '-')[[1]][1]
     choices <- sprintf("btn%d_text", 1:num_of_options)
     choice_ids <- sprintf("T%s_%04d_CHOICE%d", questionnaire, item_id, 1:num_of_options)
@@ -26,9 +33,58 @@ main_test <- function(questionnaire, label, num_items, offset = 1, arrange_verti
     )
     elts <- c(elts, item_page)
   }
+  elts <- c(elts, psychTestR::code_block(function(state, ...) {
+    results <- psychTestR::get_results(state = state, complete = FALSE)
+    score_funcs <-
+      psyquest::psyquest_item_bank %>%
+      filter(stringr::str_detect(prompt_id, stringr::str_interp("T${questionnaire}"))) %>%
+      pull(score_func)
+    subscales <-
+      psyquest::psyquest_item_bank %>%
+      filter(stringr::str_detect(prompt_id, stringr::str_interp("T${questionnaire}"))) %>%
+      pull(subscales)
+    scores_raw <- purrr::map(results, function(result) {
+      result <- get(questionnaire, results)
+      result <- as.numeric(gsub("[^0-9]", "", result))
+      result
+    })[[1]]
+    scores <-
+      purrr::map_dbl(1:length(scores_raw), function(i) {
+        eval(parse(text = score_funcs[i]))(scores_raw[i])
+      })
+
+    subscale_list = list()
+    for (i in 1:length(scores)) {
+      for (subscale in strsplit(subscales[i], ";")[[1]]) {
+        subscale_list[[subscale]] = c(subscale_list[[subscale]], scores[i])
+      }
+    }
+    postprocess(questionnaire, subscale_list, state)
+  }))
+
   psychTestR::join(psychTestR::begin_module(questionnaire),
                    elts,
                    psychTestR::end_module())
+}
+
+postprocess <- function(questionnaire = questionnaire, subscale_list = subscale_list, state = state) {
+  for (subscale in names(subscale_list)) {
+    scores <- subscale_list[[subscale]]
+
+    if(questionnaire == 'SCA' | questionnaire == 'SCS') {
+      score_mapping <- read.csv(file = stringr::str_interp("data_raw/${questionnaire}_scores.csv"),
+                                header = FALSE,
+                                sep = ";")
+      row <- score_mapping %>% filter(stringr::str_detect(V3, toString(sum(scores))))
+      value <- row[1,2]
+    } else {
+      value = mean(scores)
+    }
+
+    psychTestR::save_result(place = state,
+                            label = subscale,
+                            value = value)
+  }
 }
 
 get_prompt <- function(item_number,
