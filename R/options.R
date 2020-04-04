bb71d7ad67330e823f2b2190c385ee9c5f44cb81
Frank Höger <frank.hoeger@mirsem.org>
2020-04-04 16:37:33 +0200

    cleanup

--- R/options.R
+++
@@ -1,9 +0,0 @@
-test_options <- function() {
-  psychTestR::test_options(
-    title = "Drama Activity",
-    admin_password = "conifer",
-    researcher_email = " musicsophistication@gmail.com",
-    languages = c("EN", "DE"),
-    demo = FALSE,
-  )
-}
