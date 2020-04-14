8ee67eda767ccfc304371b06a1fad2ba65af2b4a
KilianSander <kilian.sander@aol.com>
2020-04-14 12:59:56 +0200

    added text for question 2 to 8

--- tests/testthat/test-GRT_EN.R
+++ tests/testthat/test-GRT_EN.R
@@ -15,19 +15,19 @@ app$click_next()
 
 app$expect_ui_text("Question 1 out of 8 New ideas and projects sometimes distract me from previous ones. Very much like me Mostly like me Somewhat like me Not much like me Not like me at all")
 app$click("btn1_text")
-
+app$expect_ui_text("Question 2 out of 8 Setbacks don’t discourage me. Very much like me Mostly like me Somewhat like me Not much like me Not like me at all")
 app$click("btn2_text")
-
+app$expect_ui_text("Question 3 out of 8 I have been obsessed with a certain idea or project for a short time but later lost interest. Very much like me Mostly like me Somewhat like me Not much like me Not like me at all")
 app$click("btn3_text")
-
+app$expect_ui_text("Question 4 out of 8 I am a hard worker. Very much like me Mostly like me Somewhat like me Not much like me Not like me at all")
 app$click("btn4_text")
-
+app$expect_ui_text("Question 5 out of 8 I often set a goal but later choose to pursue a different one. Very much like me Mostly like me Somewhat like me Not much like me Not like me at all")
 app$click("btn5_text")
-
+app$expect_ui_text("Question 6 out of 8 I have difficulty maintaining my focus on projects that take more than a few months to complete. Very much like me Mostly like me Somewhat like me Not much like me Not like me at all")
 app$click("btn1_text")
-
+app$expect_ui_text("Question 7 out of 8 I finish whatever I begin. Very much like me Mostly like me Somewhat like me Not much like me Not like me at all")
 app$click("btn2_text")
-
+app$expect_ui_text("Question 8 out of 8 I am diligent. Very much like me Mostly like me Somewhat like me Not much like me Not like me at all")
 app$click("btn3_text")
 
 app$expect_ui_text("Your results have been saved. You can close the browser window now.")
