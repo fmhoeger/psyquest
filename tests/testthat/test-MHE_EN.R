8b83f5fcfd6551003fb39fb3a360a22cb4600fc0
KilianSander <kilian.sander@aol.com>
2020-04-14 19:24:19 +0200

    MHE_EN test

---
+++ tests/testthat/test-MHE_EN.R
@@ -0,0 +1,68 @@
+library(psychTestR)
+library(psyquest)
+library(testthat)
+
+dir <-
+  system.file("tests/MHE_EN", package = "psyquest", mustWork = TRUE)
+app <- AppTester$new(dir)
+
+app$expect_ui_text("Please enter your ID Continue")
+app$set_inputs(p_id = "abcde")
+app$click_next()
+
+app$expect_ui_text("Please indicate the degree of current musical activity for the follow members of your family (tick all that apply). Mother …is playing an instrument …is singing in a choir Continue")
+cb <- app$findElement(css = "input[value=choice1]")
+cb$click()
+cb <- app$findElement(css = "input[value=choice2]")
+cb$click()
+app$click_next()
+
+app$expect_ui_text("Please indicate the degree of current musical activity for the follow members of your family (tick all that apply). Father …is playing an instrument …is singing in a choir Continue")
+cb <- app$findElement(css = "input[value=choice1]")
+cb$click()
+cb <- app$findElement(css = "input[value=choice2]")
+cb$click()
+app$click_next()
+
+app$expect_ui_text("During holiday times and weekends, how often do your parents or someone else at home do the following activities with you? Help me with my homework Every day or nearly every day Once or twice per week Once or twice per month Never or hardly ever Continue")
+cb <- app$findElement(css = "input[value=choice1]")
+cb$click()
+app$click_next()
+
+app$expect_ui_text("During holiday times and weekends, how often do your parents or someone else at home do the following activities with you? Talk with me about a topic we've learned about in school Every day or nearly every day Once or twice per week Once or twice per month Never or hardly ever Continue")
+cb <- app$findElement(css = "input[value=choice2]")
+cb$click()
+app$click_next()
+
+app$expect_ui_text("During holiday times and weekends, how often do your parents or someone else at home do the following activities with you? Talk about school related problems Every day or nearly every day Once or twice per week Once or twice per month Never or hardly ever Continue")
+cb <- app$findElement(css = "input[value=choice3]")
+cb$click()
+app$click_next()
+
+app$expect_ui_text("During holiday times and weekends, how often do your parents or someone else at home do the following activities with you? Encourage me to make music Every day or nearly every day Once or twice per week Once or twice per month Never or hardly ever Continue")
+cb <- app$findElement(css = "input[value=choice4]")
+cb$click()
+app$click_next()
+
+app$expect_ui_text("During holiday times and weekends, how often do your parents or someone else at home do the following activities with you? Support me with practicing my instrument Every day or nearly every day Once or twice per week Once or twice per month Never or hardly ever Continue")
+cb <- app$findElement(css = "input[value=choice1]")
+cb$click()
+app$click_next()
+
+results <- app$get_results() %>% as.list()
+expect_equal(names(results), c("MHE"))
+expect_equal(
+  results[["MHE"]],
+  list(
+    q1 = "choice1,choice2",
+    q2 = "choice1,choice2",
+    q3 = "choice1",
+    q4 = "choice2",
+    q5 = "choice3",
+    q6 = "choice4",
+    q7 = "choice1",
+    General = 1.6964507
+  )
+)
+
+app$stop()
