d69f9e655fafb010e4f932f505f446392bab70ab
Viola <viola.pausch@hmtm-hannover.de>
2020-04-22 19:08:15 +0200

    test-CCM low boundary condition

--- tests/testthat/test-CCM_EN.R
+++ tests/testthat/test-CCM_EN.R
@@ -11,33 +11,35 @@ app$set_inputs(p_id = "abcde")
 app$click_next()
 
 app$expect_ui_text("Please read the following list of musical activities and select the ones that you have done during the last three months. Please tick all that apply. play in an orchestra sometimes play music with friends sometimes make music at events or special occasions receive individual lessons on an instrument (or voice) receive group lessons on an instrument (or voice) have music classes in school attend music-related after school clubs sometimes compile playlists for myself or others none of the above Continue")
-cb <- app$findElement(css = "input[value=choice1]")
-cb$click()
-cb <- app$findElement(css = "input[value=choice3]")
-cb$click()
-cb <- app$findElement(css = "input[value=choice5]")
-cb$click()
-cb <- app$findElement(css = "input[value=choice7]")
+# cb <- app$findElement(css = "input[value=choice1]")
+# cb$click()
+# cb <- app$findElement(css = "input[value=choice3]")
+# cb$click()
+# cb <- app$findElement(css = "input[value=choice5]")
+# cb$click()
+# cb <- app$findElement(css = "input[value=choice7]")
+# cb$click()
+cb <- app$findElement(css = "input[value=choice9]")
 cb$click()
 app$click_next()
 
 app$expect_ui_text("In the last three months I had 0 hours of music class per week 1 hour of music class per week 2 hours of music class per week 3 hours of music class per week 4 or more hours of music class per week Continue")
-rb <- app$findElement(css = "input[value=choice2]")
+rb <- app$findElement(css = "input[value=choice1]")
 rb$click()
 app$click_next()
 
 app$expect_ui_text("In the last three months I had 0 hours of music-related after school class per week 1 hour of music-related after school class per week 2 hours of music-related after school class per week 3 hours of music-related after school class per week 4 or more hours of music-related after school class per week Continue")
-rb <- app$findElement(css = "input[value=choice3]")
+rb <- app$findElement(css = "input[value=choice1]")
 rb$click()
 app$click_next()
 
 app$expect_ui_text("How much do you currently practice your instrument(s)? 0 minutes per day 0-15 minutes per day 15-30 minutes per day 30-60 minutes per day 1-1.5 hours per day 1.5-2 hours per day more than 2 hours per day Continue")
-rb <- app$findElement(css = "input[value=choice4]")
+rb <- app$findElement(css = "input[value=choice1]")
 rb$click()
 app$click_next()
 
 app$expect_ui_text("Taken all together (own practice, rehearsals, instrument lessons, gigs, etc.) how much time do you spend making music per week at the moment? 0 hours per week 0-1 hours per week 1-2 hours per week 2-4 hours per week 4-7 hours per week 7-14 hours per week more than 14 hours per week Continue")
-rb <- app$findElement(css = "input[value=choice5]")
+rb <- app$findElement(css = "input[value=choice1]")
 rb$click()
 app$click_next()
 
@@ -46,13 +48,13 @@ expect_equal(names(results), c("CCM"))
 expect_equal(
   results[["CCM"]],
   list(
-    q1 = "choice1,choice3,choice5,choice7",
-    q2 = "choice2",
-    q3 = "choice3",
-    q4 = "choice4",
-    q5 = "choice5",
-    General = 3.7918376,
-    Extra =2.5
+    q1 = "choice9",
+    q2 = "choice1",
+    q3 = "choice1",
+    q4 = "choice1",
+    q5 = "choice1",
+    General = -2.5421857,
+    Extra =1
   )
 )
 
