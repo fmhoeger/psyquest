72dd97cdda4eae611637100becbda710e68d370f
Viola <viola.pausch@hmtm-hannover.de>
2020-05-03 19:24:24 +0200

    test-TOM: Subscales Entitiy, stable and gift low boundary condition

---
+++ tests/testthat/test-TOM_DE_subscales_Stable_Entity_Gift_minimum.R
@@ -0,0 +1,46 @@
+library(psychTestR)
+library(psyquest)
+library(testthat)
+
+dir <-
+  system.file("tests/TOM_DE_subscales_Stable_Entity_Gift", package = "psyquest", mustWork = TRUE)
+app <- AppTester$new(dir)
+
+# Enter id
+app$expect_ui_text("Bitte gib Deine ID ein Weiter")
+app$set_inputs(p_id = "abcde")
+app$click_next()
+
+# Intro
+app$expect_ui_text("Wir interessieren uns für deine Meinung zu musikalischen Fähigkeiten. Lies die folgenden Sätze und wähle aus, wie sehr du mit der Aussage übereinstimmst. Es gibt keine richtigen oder falschen Antworten. Weiter")
+app$click_next()
+
+app$expect_ui_text("Frage 1 von 6 Man hat ein gewisses Level an musikalischen Fähigkeiten und kann nicht viel tun, um das zu ändern. Stimme überhaupt nicht zu Stimme nicht zu Weder noch Stimme zu Stimme sehr zu")
+app$click("btn1_text")
+
+app$click("btn1_text")
+app$click("btn1_text")
+app$click("btn1_text")
+app$click("btn1_text")
+app$click("btn1_text")
+
+app$expect_ui_text("Deine Ergebnisse wurden gespeichert. Du kannst das Browserfenster jetzt schließen.")
+
+results <- app$get_results() %>% as.list()
+expect_equal(names(results), c("TOM"))
+expect_equal(
+  results[["TOM"]],
+  list(
+    q1 = "btn1_text",
+    q3 = "btn1_text",
+    q4 = "btn1_text",
+    q7 = "btn1_text",
+    q10 = "btn1_text",
+    q11 = "btn1_text",
+    Entity = 1,
+    Stable = 1,
+    Gift = 1
+  )
+)
+
+app$stop()
