a4210a3fb6bb436caf96066b7ef9fdf808bd7d36
Frank Höger <fmhoeger@mirsem.org>
2020-06-29 13:29:24 +0200

    update CMT dictionary

--- tests/testthat/test-CMT_de_minimum.R
+++ tests/testthat/test-CMT_de_minimum.R
@@ -9,13 +9,13 @@ app$expect_ui_text("Bitte gib Deine ID ein Weiter")
 app$set_inputs(p_id = "abcde")
 app$click_next()
 
-app$expect_ui_text("In diesem Fragebogen geht es um DEINEN Musikunterricht in der Schule. Du wirst nach verschiedenen Tätigkeiten im Musikunterricht gefragt. Versuche Dich für jede Tätigkeit so gut es geht zu erinnern: Wie oft hast DU diese Tätigkeit gemacht? Vielleicht hast Du sie in keiner, vielleicht in mehr als zehn Unterrichtsstunden (45min) gemacht. Es gibt kein richtig und falsch. Wenn du dich nicht mehr erinnern kannst, entscheide einfach aus dem Bauch. Weiter")
+app$expect_ui_text("In diesem Fragebogen geht es um Deinen Musikunterricht in der Schule. Du wirst nach verschiedenen Tätigkeiten im Musikunterricht gefragt. Versuche Dich für jede Tätigkeit so gut es geht zu erinnern: Wie oft hast Du diese Tätigkeit gemacht? Vielleicht hast Du sie in keiner, vielleicht in mehr als zehn Unterrichtsstunden (45min) gemacht. Es gibt kein richtig und falsch. Wenn du dich nicht mehr erinnern kannst, entscheide einfach aus dem Bauch. Weiter")
 app$click_next()
 
-app$expect_ui_text("Frage 1 von 12 Wie oft hast DU in den letzten 6 Monaten (vor Corona) im Musikunterricht… Kreuze an! …Lieder mit Instrumenten begleitet? nie in 1 Unterrichtsstunde in 2 bis 5 Unterrichtsstunden in 6 bis 10 Unterrichtsstunden in mehr als 10 Unterrichtsstunden")
+app$expect_ui_text("Frage 1 von 12 Wie oft hast Du in den letzten 6 Monaten (vor Corona) im Musikunterricht… …Lieder mit Instrumenten begleitet? nie in 1 Unterrichtsstunde in 2 bis 5 Unterrichtsstunden in 6 bis 10 Unterrichtsstunden in mehr als 10 Unterrichtsstunden")
 app$click("btn1_text")
 
-app$expect_ui_text("Frage 2 von 12 Wie oft hast DU in den letzten 6 Monaten (vor Corona) im Musikunterricht… Kreuze an! …verschiedene Tänze ausgeführt? nie in 1 Unterrichtsstunde in 2 bis 5 Unterrichtsstunden in 6 bis 10 Unterrichtsstunden in mehr als 10 Unterrichtsstunden")
+app$expect_ui_text("Frage 2 von 12 Wie oft hast Du in den letzten 6 Monaten (vor Corona) im Musikunterricht… …verschiedene Tänze ausgeführt? nie in 1 Unterrichtsstunde in 2 bis 5 Unterrichtsstunden in 6 bis 10 Unterrichtsstunden in mehr als 10 Unterrichtsstunden")
 app$click("btn1_text")
 
 app$click("btn1_text")
@@ -28,7 +28,7 @@ app$click("btn1_text")
 app$click("btn1_text")
 app$click("btn1_text")
 
-app$expect_ui_text("Frage 12 von 12 Wie oft hast DU in den letzten 6 Monaten (vor Corona) im Musikunterricht… Kreuze an! …Deine eigenen Fähigkeiten beim Musik machen mit anderen eingeschätzt? nie in 1 Unterrichtsstunde in 2 bis 5 Unterrichtsstunden in 6 bis 10 Unterrichtsstunden in mehr als 10 Unterrichtsstunden")
+app$expect_ui_text("Frage 12 von 12 Wie oft hast Du in den letzten 6 Monaten (vor Corona) im Musikunterricht… …Deine eigenen Fähigkeiten beim Musik machen mit anderen eingeschätzt? nie in 1 Unterrichtsstunde in 2 bis 5 Unterrichtsstunden in 6 bis 10 Unterrichtsstunden in mehr als 10 Unterrichtsstunden")
 app$click("btn1_text")
 
 
