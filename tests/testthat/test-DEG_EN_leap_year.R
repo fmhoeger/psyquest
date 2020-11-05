578d3c376d0da1834cb18fe5056dbae3773c34bb
Viola <viola.pausch@hmtm-hannover.de>
2020-11-05 21:29:58 +0100

    adapt `age´ to current month (11/2020)

--- tests/testthat/test-DEG_EN_leap_year.R
+++ tests/testthat/test-DEG_EN_leap_year.R
@@ -68,7 +68,7 @@ expect_equal(
     'Hearing Impairment' = 1,
     'Type of Hearing Impairment' = "Tinnitus",
     Gender = 1,
-    Age = 248,
+    Age = 249,
     Nationality = "UK",
     'Country Formative Years' = "UK",
     'First Language' = "ar",
