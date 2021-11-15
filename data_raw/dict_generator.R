4b426a474df10043657adbc7408d60b91a5c8659
klaus <kgf@omniversum.de>
2021-11-15 21:10:52 +0100

    de_f version for DEG

--- data_raw/dict_generator.R
+++ data_raw/dict_generator.R
@@ -3,8 +3,12 @@ library(tidyverse)
 psyquest_dict_raw <-
   map_dfr(list.files("./data_raw/dicts/", full.names = TRUE), function(filepath) {
     #dict file must be UTF8 encoded!
-    read.table(filepath, sep = ";", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "utf8") %>%
-      filter(nchar(de) != 0, nchar(en) != 0) %>% mutate(de_f = de)
+    tmp <- read.table(filepath, sep = ";", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "utf8") %>%
+      filter(nchar(de) != 0, nchar(en) != 0)
+    if(!("de_f" %in% names(tmp))){
+      tmp <- tmp  %>% mutate(de_f = de)
+    }
+    tmp
   })
 
 psyquest_dict <- psychTestR::i18n_dict$new(psyquest_dict_raw)
