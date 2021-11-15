4fe0b65fabebd880ce9330fba8884e7adda6bef8
klaus <kgf@omniversum.de>
2021-11-15 18:34:06 +0100

    Added de_f, changed some FSS wordings

--- data_raw/dict_generator.R
+++ data_raw/dict_generator.R
@@ -4,7 +4,7 @@ psyquest_dict_raw <-
   map_dfr(list.files("./data_raw/dicts/", full.names = TRUE), function(filepath) {
     #dict file must be UTF8 encoded!
     read.table(filepath, sep = ";", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "utf8") %>%
-      filter(nchar(de) != 0, nchar(en) != 0)
+      filter(nchar(de) != 0, nchar(en) != 0) %>% mutate(de_f = de)
   })
 
 psyquest_dict <- psychTestR::i18n_dict$new(psyquest_dict_raw)
