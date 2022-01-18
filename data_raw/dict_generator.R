3c56089ae3764f7b5f983e8b18fea080811d0af5
KilianSander <kilian.sander@aol.com>
2022-01-18 18:36:24 +0100

    dict generator changes from Klaus

--- data_raw/dict_generator.R
+++ data_raw/dict_generator.R
@@ -2,8 +2,13 @@ library(tidyverse)
 
 psyquest_dict_raw <-
   map_dfr(list.files("./data_raw/dicts/", full.names = TRUE), function(filepath) {
-    read.csv(filepath, sep = ";", stringsAsFactors = FALSE, header = TRUE) %>%
+    #dict file must be UTF8 encoded!
+    tmp <- read.table(filepath, sep = ";", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "utf8") %>%
       filter(nchar(de) != 0, nchar(en) != 0)
+    if(!("de_f" %in% names(tmp))){
+      tmp <- tmp  %>% mutate(de_f = de)
+    }
+    tmp
   })
 
 psyquest_dict <- psychTestR::i18n_dict$new(psyquest_dict_raw)
