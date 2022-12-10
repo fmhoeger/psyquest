2c0b5e5f1e788acf7a073785c23d785d4602b994
klaus <kgf@omniversum.de>
2022-12-10 01:27:29 +0100

    Server debug

---
+++ R/MDS.R
@@ -0,0 +1,57 @@
+#' MDS
+#'
+#' This function defines a Music Dislikes Scale (MDS) module for incorporation into a
+#' psychTestR timeline.
+#' Use this function if you want to include the MDS in a
+#' battery of other tests, or if you want to add custom psychTestR
+#' pages to your test timeline.
+#' For a standalone implementation of the MDS,
+#' consider using \code{\link{MDS_standalone}()}.
+#' @param label (Character scalar) Three uppercase letter acronym of the questionnaire.
+#' This is also the label given to the results in the output file.
+#' @param target (Character scalar or named character vector) The main target (style, artist...) for the questionnaire, if different values for different lanugages are needed then it should be a named vector with the names according to language columns in the dict.
+#' @param dict (i18n_dict) The psyquest dictionary used for internationalisation.
+#' @param ... Further arguments to be passed to \code{\link{MDS}()}.
+#' @export
+MDS <- function(label = "MDS",
+                target = c("en" = "a music", "de" = "einer Musik", "de_f" = "einer Musik"),
+                dict = psyquest::psyquest_dict,
+                ...) {
+  stopifnot(purrr::is_scalar_character(label))
+  questionnaire_id <- "MDS"
+  #browser()
+  dict_raw <- dict %>% as_tibble()
+  if(length(target) > 1){
+    if(is.null(names(target))){
+      stop("Target vector must be named if it has more than one element")
+    }
+    common_names <- intersect(names(dict_raw), names(target))
+    if(length(common_names) != length(target)){
+      warning("Target does not contain all dictionary languages.")
+    }
+    target <- target[common_names]
+    if(length(target) == 0){
+      stop("All target names invalid.")
+    }
+  }
+  else{
+    target <- rep(target, length(dict_raw)-1)
+    names(target) <- setdiff(names(dict_raw), "key")
+  }
+  prompt <- dict_raw[dict_raw$key  == "TMDS_0001_PROMPT", names(target)] %>% as.character()
+  fixed_prompt <- map_chr(seq_along(names(target)),
+                          function(i){
+                            str_replace(prompt[[i]], fixed("{{target}}"), target[i])
+                            })
+  dict_raw[dict_raw$key  == "TMDS_0001_PROMPT", names(target)] <- as.list(fixed_prompt)
+  patch_dict <-  psychTestR::i18n_dict$new(dict_raw)
+  main_test(
+    questionnaire_id = questionnaire_id,
+    label = label,
+    items = get_items(questionnaire_id),
+    offset = 1,
+    arrange_vertically = FALSE,
+    button_style = "min-width: 290px;width: 290px",
+    dict = patch_dict
+  )
+}
